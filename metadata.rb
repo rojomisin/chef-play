99a9be817eed2b6ff621834b5cb3c2e2a58509f7
Dennis Hoer <dennis.hoer@pearson.com>
2015-12-08 01:11:14 -0500

    Add source and issues url

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'play'
 maintainer 'Dennis Hoer'
 maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
-description 'Installs/Configures distribution artifact as a service.'
+description 'Installs/Configures Play distribution artifact as a service.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
@@ -11,3 +11,6 @@ supports 'redhat'
 supports 'ubuntu'
 
 depends 'ark', '~> 0.9'
+
+source_url 'https://github.com/dhoer/chef-play' if respond_to?(:source_url)
+issues_url 'https://github.com/dhoer/chef-play/issues' if respond_to?(:issues_url)
