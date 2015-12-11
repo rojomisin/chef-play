caadd124a8e0720d67e79b4dd3bf11e7c0ebca41
Dennis Hoer <dennis.hoer@pearson.com>
2015-12-11 14:04:39 -0700

    Suppress FC021: Resource condition in provider may not behave as expected

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennis.hoer@gmail.com'
 license 'MIT'
 description 'Installs/Configures Play distribution artifact as a service.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.1'
 
 supports 'centos'
 supports 'redhat'
