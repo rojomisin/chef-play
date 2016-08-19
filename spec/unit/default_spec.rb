3a5d38b82d8a26e83906e8a00644dbdbb46ae4f4
Dennis Hoer <dennis.hoer@pearson.com>
2016-08-19 14:27:45 -0700

    Update integration test

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
-SOURCE = 'https://github.com/dhoer/play-java-sample/releases/download/1.0/play-java-sample-1.0.zip'.freeze
-
 describe 'play_test::default' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new(
       platform: 'centos', version: '7.0', step_into: ['play'], file_cache_path: CACHE
     ).converge(described_recipe)
   end
-  # let(:play_service_template) { chef_run.template('/etc/init.d/sample_service') }
+  let(:play_service_template) { chef_run.template('/etc/init.d/play-java-sample') }
 
   it 'installs play app' do
-    expect(chef_run).to install_play(SOURCE).with(
+    expect(chef_run).to install_play(
+      'https://github.com/dhoer/play-java-sample/releases/download/1.0/play-java-sample-1.0.zip'
+    ).with(
       conf_variables:  { secret: 'mysecret' }
     )
   end
@@ -20,59 +20,59 @@ describe 'play_test::default' do
     expect(chef_run).to install_package('unzip')
   end
 
-  it 'creates group' do
-    expect(chef_run).to create_group('play')
-  end
-
   it 'creates user' do
     expect(chef_run).to create_user('play')
   end
 
+  it 'creates group' do
+    expect(chef_run).to create_group('play')
+  end
+
   it 'creates path' do
-    expect(chef_run).to create_directory('/opt/play')
+    expect(chef_run).to create_directory('/opt/play/zip')
   end
 
   it 'downloads app' do
     expect(chef_run).to create_remote_file("#{CACHE}/play-java-sample-1.0.zip")
   end
 
+  it 'does not untar zip' do
+    expect(chef_run).to_not run_execute("untar #{CACHE}/play-java-sample-1.0.zip")
+  end
+
+  it 'unzips zip' do
+    expect(chef_run).to_not run_execute("unzip #{CACHE}/play-java-sample-1.0.zip")
+  end
+
+  it 'creates link' do
+    expect(chef_run).to create_link('/opt/play/zip/play-java-sample')
+  end
+
   it 'verifies executable exists' do
     expect(chef_run).to run_ruby_block('verify executable exists')
   end
 
-  #
-  #   it 'does not install sample_service as a service' do
-  #     expect(chef_run).to_not enable_service(SERVICENAME)
-  #     expect(chef_run).to_not restart_service(SERVICENAME)
-  #     expect(chef_run).to_not stop_service(SERVICENAME)
-  #     expect(chef_run).to_not start_service(SERVICENAME)
-  #   end
-  #
-  #   it 'does not register sample_service to start on reboot' do
-  #     expect(chef_run).to_not run_execute('chkconfig sample_service')
-  #   end
-  #
   it 'makes play script executable' do
     expect(chef_run).to run_execute('make play script executable')
   end
-  #
-  #   it 'creates init.d script' do
-  #     expect(chef_run).to create_template('/etc/init.d/sample_service').with(
-  #       user: 'root',
-  #       group: 'root',
-  #       backup: 5
-  #     )
-  #   end
-  #
-  #   it 'sends notifications if init.d script created' do
-  #     expect(play_service_template).to notify('service[sample_service]').to(:enable)
-  #     expect(play_service_template).to notify('service[sample_service]').to(:start)
-  #   end
-  #
-  #   it 'creates application.conf' do
-  #     expect(chef_run).to create_template('/usr/local/sample_service/conf/application.conf').with(
-  #       user: 'play'
-  #     )
-  #   end
-  # end
+
+  it 'configures application.conf' do
+    expect(chef_run).to create_template('/opt/play/zip/play-java-sample/conf/application.conf')
+  end
+
+  it 'creates pid dir' do
+    expect(chef_run).to create_directory('/var/run/play-java-sample')
+  end
+
+  it 'restarts service' do
+    expect(play_service_template).to notify('service[play-java-sample]').to(:restart)
+  end
+
+  it 'creates service script' do
+    expect(chef_run).to create_template('/etc/init.d/play-java-sample')
+  end
+
+  it 'enables service script' do
+    expect(chef_run).to enable_service('play-java-sample')
+  end
 end
--- test/fixtures/cookbooks/play_test/recipes/default.rb
+++ test/fixtures/cookbooks/play_test/recipes/default.rb
@@ -6,4 +6,5 @@ play 'https://github.com/dhoer/play-java-sample/releases/download/1.0/play-java-
   conf_source 'conf/application.conf.erb'
   conf_variables(secret: 'mysecret')
   args %w(-J-Xms128M -J-Xmx512m -J-server)
+  path '/opt/play/zip'
 end
