0071a3817b546fa14e1fb905b5537663780ad09e
Dennis Hoer <dennis.hoer@pearson.com>
2016-08-20 15:54:24 -0700

    Fix fedora port test

--- test/integration/default/serverspec/tarball_spec.rb
+++ test/integration/default/serverspec/tarball_spec.rb
@@ -93,8 +93,8 @@ describe 'dist tgz' do
     it { should be_running }
   end
 
-  describe port(8080) do
-    it { should be_listening } unless os[:family] == 'redhat'
+  describe port(9000), if: !%w(redhat fedora).include?(os[:family]) do
+    it { should be_listening }
   end
 
   describe command('wget -O - localhost:8080') do
