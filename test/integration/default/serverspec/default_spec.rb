9e5b7e69cd6fe9a1486a9749369daf2c9b167244
Dennis Hoer <dennis.hoer@pearson.com>
2016-08-19 17:59:00 -0700

    Fix systemd issues

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -56,12 +56,12 @@ describe 'dist zip' do
       it { should be_mode 755 }
       it { should be_owned_by 'root' }
       it { should be_grouped_into 'root' }
-      its(:content) { should match(%r{PLAY_DIST_HOME="/usr/local/sample_service"}) }
+      its(:content) { should match(%r{PLAY_DIST_HOME="/local/sample_service"}) }
       its(:content) { should match(%r{PLAY="\$\{PLAY_DIST_HOME\}/bin/play-java-sample"}) }
       its(:content) { should match(/USER="play"/) }
       its(:content) { should match(%r{PID_PATH="/var/run/play"}) }
       its(:content) { should match(%r{PID_FILE="\$\{PID_PATH\}/sample_service.pid"}) }
-      its(:content) { should match(%r{CONFIG_FILE="/usr/local/sample_service/conf/application.conf"}) }
+      its(:content) { should match(%r{CONFIG_FILE="/local/sample_service/conf/application.conf"}) }
       its(:content) { should match(/APP_ARGS="-Dhttp\.port=8080 -J-Xms128M -J-Xmx512m -J-server"/) }
       its(:content) { should match(%r{su -s /bin/sh \$\{USER\} -c "\( \$\{PLAY\} -Dpidfile\.path=\$\{PID_FILE\}}) }
       its(:content) { should match(/-Dconfig\.file=\$\{CONFIG_FILE\} \$\{APP_ARGS\} &\ \)/) }
@@ -77,8 +77,8 @@ describe 'dist zip' do
       its(:content) { should match(%r{WorkingDirectory=/opt/play/zip/play-java-sample}) }
       its(:content) { should match(/User=play/) }
       its(:content) { should match(/Group=play/) }
-      its(:content) { should match(%r{ExecStartPre=-/usr/bin/mkdir /var/run/play-java-sample}) }
-      its(:content) { should match(%r{ExecStartPre=/usr/bin/chown -R play:play /var/run/play-java-sample}) }
+      its(:content) { should match(%r{ExecStartPre=/bin/mkdir /var/run/play-java-sample}) }
+      its(:content) { should match(%r{ExecStartPre=/bin/chown -R play:play /var/run/play-java-sample}) }
       its(:content) do
         should match(%r{ExecStart=/opt/play/zip/play-java-sample/bin/play-java-sample \
 -Dpidfile.path=/var/run/play-java-sample/play.pid -Dconfig.file=/opt/play/zip/play-java-sample/conf/application.conf \
